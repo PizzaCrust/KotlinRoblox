fe5158820b748a9daabd3409f6c63c5c1f8c4771
PizzaCrust <pizzacrustcoder@gmail.com>
2018-08-05 18:27:59 +0700

    Profile can be created with a UserId

--- src/main/kotlin/online/pizzacrust/kotlinroblox/std/StdProfile.kt
+++ src/main/kotlin/online/pizzacrust/kotlinroblox/std/StdProfile.kt
@@ -4,28 +4,48 @@ import online.pizzacrust.kotlinroblox.api.Profile
 import online.pizzacrust.kotlinroblox.implementation
 
 fun profile(username: String): Profile? {
-    try {
-        return StdProfile(username)
+    return try {
+        StdProfile(username)
     } catch (e: RuntimeException) {
-        return null
+        null
     }
 }
 
-class StdProfile @Deprecated("Do not use this without catching it.") constructor(username: String) : Profile {
+fun profile(userId: Long): Profile? {
+    return try {
+        StdProfile(userId)
+    } catch (e: RuntimeException) {
+        null
+    }
+}
+
+class StdProfile : Profile {
 
     private val internalId: Long
-    private val internalUsername: String = username
+    private val internalUsername: String
 
-    data class UsernameToIdResponse(var userId: Long?) {
-        constructor(): this(null)
+    data class UsernameToIdResponse(var userId: Long?, var username: String?) {
+        constructor(): this(null, null)
     }
 
-    init {
+    @Deprecated("Do not use this without catching it.")
+    constructor(username: String) {
         val url = "http://api.roblox.com/users/get-by-username?username=$username"
         val responseText = implementation.reqManager.get(url).response
         val parsed: UsernameToIdResponse = implementation.jsonManager.fromJson(responseText,
                 UsernameToIdResponse()) as UsernameToIdResponse
         this.internalId = parsed.userId ?: throw RuntimeException("invalid username")
+        this.internalUsername = username
+    }
+
+    @Deprecated("Do not use this without catching it.")
+    constructor(id: Long) {
+        val url = "http://api.roblox.com/users/$id"
+        val responseText = implementation.reqManager.get(url).response
+        val parsed: UsernameToIdResponse = implementation.jsonManager.fromJson(responseText,
+                UsernameToIdResponse()) as UsernameToIdResponse
+        this.internalUsername = parsed.username ?: throw RuntimeException("invalid id")
+        this.internalId = id
     }
 
     override val id: Long
